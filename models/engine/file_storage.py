1fb48e74f6263bbda8d3dfb9fee3a203ca226ea9
ibkam <110990848+ibkam@users.noreply.github.com>
2024-03-24 18:12:49 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -64,10 +64,9 @@ class FileStorage:
         self.reload()
 
     def delete(self, obj=None):
-    """
-    Delete obj from __objects if it’s inside. If obj is None, the method should not do anything.
-    """
-    if obj:
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        if key in self.__objects:
-            del self.__objects[key]
+        """
+            delete obj from __objects if it’s inside - if obj is None,
+            the method should not do anything
+        """
+        if (obj):
+            self.__objects.pop("{}.{}".format(type(obj).__name__, obj.id))
