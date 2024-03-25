9e1a0c87b2318c94412968c99e988cbba6a893f6
ibkam <110990848+ibkam@users.noreply.github.com>
2024-03-25 11:57:19 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -4,8 +4,20 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    """
+        User class for the user
+        User class for the user
+    """
+    if (storage_engine == 'db'):
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
