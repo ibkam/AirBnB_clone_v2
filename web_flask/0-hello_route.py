f3f6a8690949d15168fcd077bb37cb3c5a5e72cf
ibkam <110990848+ibkam@users.noreply.github.com>
2024-05-07 15:29:16 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def display():
-  """returns Hello HBNB!"""
+def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
